35db7c973d4983d541979495d00265bb4ee33bb2
JVN313 <82493100+JVN313@users.noreply.github.com>
2021-10-05 18:16:30 -0400

    Adding Game Files

---
+++ main.py
@@ -0,0 +1,76 @@
+import random
+
+# Game Variables
+playing = True
+player_wins = 0
+computer_wins = 0
+draws = 0
+games_played = 0
+error_count = 0
+options = ["ROCK","PAPER","SCISSORS"]
+answer = options[random.randrange(0,3)]
+user_input = input("Type Rock, Paper, Scissors, or Q to quit the game: ").upper()
+
+# Game Functions
+def player_win():
+    global player_wins
+    player_wins+=1
+    print("You Won!")
+
+def replay():
+    global answer,user_input
+    answer = options[random.randrange(0,3)]
+    user_input = input("To play again type Rock, Paper, Scissors, or Q to quit the game: ").upper()
+
+def score_board():
+    global score_entry
+    score_board_add = open("RPSscoreboard.txt","a")
+    score_entry = input("ENTER YOUR NAME TO BE PLACED ON THE LEADERBOARDS: ").upper()
+    if score_entry == "Q" or score_entry == "QUIT":
+        quit()
+    score_board_add.write("\n"+score_entry+" "+str(player_wins)+"W"+"/"+str(computer_wins)+"L"+"/"+str(draws)+"D")
+    score_board_add.close()
+    score_board_show = open("RPSscoreboard.txt","r")
+    print(score_board_show.read())
+    score_board_show.close()
+
+def log_error():
+    global score_entry
+    log_error_add = open("ErrorLog.txt", "a")
+    log_error_add.write("\nUser "+score_entry+" Had " +str(error_count)+" Errors During Their Play")
+
+# Game Loop
+while playing:
+    if user_input == "Q" or user_input =="QUIT":
+        playing = False
+    elif user_input == options[1] and answer == options[0]:
+        games_played+=1
+        player_win()
+        replay()
+    elif user_input == options[0] and answer == options[2]:
+        games_played+=1
+        player_win()
+        replay()
+    elif user_input == options[2] and answer == options[1]:
+        games_played+=1
+        player_win()
+        replay()
+    elif user_input not in options:
+        error_count+=1
+        print("Invalid Input")
+        replay()
+    elif user_input==answer:
+        games_played+=1
+        draws+=1
+        print("DRAW!")
+        replay()
+    else:
+        games_played+=1
+        computer_wins+=1
+        print("Sorry You Lost :( ")
+        replay()
+    
+print("You Won "+str(player_wins)+" Time(s), Lost "+str(computer_wins)+" Time(s), And Had " +str(draws)+" Draw(s)!")
+score_board()
+log_error()
+print("Thanks For Playing!")
